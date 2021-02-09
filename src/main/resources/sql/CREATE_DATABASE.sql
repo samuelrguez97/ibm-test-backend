bf050366e5229b080a871b79dab96992b85fa74c
samuel <samuelrguez97@gmail.com>
2021-02-09 08:11:01 +0100

    sql changes

--- src/main/resources/sql/CREATE_DATABASE.sql
+++ src/main/resources/sql/CREATE_DATABASE.sql
@@ -1,4 +1,3 @@
 create database db_ibmtest;
 create user 'springapiibm'@'%' identified by 'ibmtest';
 grant all on db_ibmtest.* to 'springapiibm'@'%';
-commit;
--- src/main/resources/sql/CREATE_TABLE.sql
+++ src/main/resources/sql/CREATE_TABLE.sql
@@ -7,4 +7,3 @@ CREATE TABLE `supplier` (
  `score` int(11) DEFAULT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
-commit;
